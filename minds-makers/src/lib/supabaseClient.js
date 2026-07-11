1980a8f095c08a5e7b41f82d0c2a0ba377065a53
Minds-Makers <nix0abd0zero0@gmail.com>
2026-07-11 22:42:29 +0300

    Update supabaseClient.js

--- minds-makers/src/lib/supabaseClient.js
+++ minds-makers/src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'letdkskgdbuyrqdaxccx.supabase.co'
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3bmhobnd6Y2RlZ2xxa2xkZ252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNDE5MDUsImV4cCI6MjA2MTYxNzkwNX0.VAvUfdcRoLYNLiNJx_4wj1CDgDmvm8leRS0JjSXdi4c'
+const SUPABASE_URL = 'https://letdkskgdbuyrqdaxccx.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxldGRrc2tnZGJ1eXJxZGF4Y2N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzgwMjIsImV4cCI6MjA5OTM1NDAyMn0.IMPMBLrFoFkvJgXVZ15Iy8n7hWm67h-SZa0cceMnmTI'
 
 export const isSupabaseConfigured = true
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
