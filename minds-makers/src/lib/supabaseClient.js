300028660644a7f3a6211a5f5673befdb9a35ffd
Minds-Makers <nix0abd0zero0@gmail.com>
2026-07-11 22:39:35 +0300

    Update supabaseClient.js

--- minds-makers/src/lib/supabaseClient.js
+++ minds-makers/src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://wwnhhnwzcdeglqkldgnv.supabase.co'
+const SUPABASE_URL = 'letdkskgdbuyrqdaxccx.supabase.co'
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3bmhobnd6Y2RlZ2xxa2xkZ252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNDE5MDUsImV4cCI6MjA2MTYxNzkwNX0.VAvUfdcRoLYNLiNJx_4wj1CDgDmvm8leRS0JjSXdi4c'
 
 export const isSupabaseConfigured = true
